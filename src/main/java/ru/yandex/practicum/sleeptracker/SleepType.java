28fa672e343f15f46a9e9e472adcc99ebd329fe6
9138521930pv-prog <9138521930.pv@gmail.com>
2025-12-28 18:29:46 +0700

    Доработал код удрал цикл FOR чтоб соответствовало ТЗ

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepType.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepType.java
@@ -2,9 +2,13 @@ package ru.yandex.practicum.sleeptracker;
 
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class SleepType implements SleepAnalyzer {
 
+    @Override
     public AnalysisResult apply(List<SleepSession> sessions) {
         var nightList = sessions.stream()
                 .filter(SleepSession::isNightSleep)
@@ -14,31 +18,38 @@ public class SleepType implements SleepAnalyzer {
             return new AnalysisResult("Тип сна", "Голубь");
         }
 
-        long sov = 0;
-        long javor = 0;
-        long golub = 0;
+        // Группируем сеансы по типу и считаем количество в каждой группе
+        Map<String, Long> typeCounts = nightList.stream()
+                .map(this::classifySession)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting()
+                ));
 
-        for (var session : nightList) {
-            LocalTime start = session.start().toLocalTime();
-            LocalTime end = session.end().toLocalTime();
+        // Определяем тип с максимальным количеством
+        String dominantType = Map.of(
+                        "Сова", typeCounts.getOrDefault("Сова", 0L),
+                        "Жаворонок", typeCounts.getOrDefault("Жаворонок", 0L),
+                        "Голубь", typeCounts.getOrDefault("Голубь", 0L)
+                ).entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse("Голубь"); // запасной вариант (маловероятен)
 
-            if (start.isAfter(LocalTime.of(23, 0)) && end.isAfter(LocalTime.of(9, 0))) {
-                sov++;
-            }
-            else if (start.isBefore(LocalTime.of(22, 0)) && end.isBefore(LocalTime.of(7, 0))) {
-                javor++;
-            }
-            else {
-                golub++;
-            }
-        }
+        return new AnalysisResult("Тип сна", dominantType);
+    }
 
-        if (sov > javor && sov > golub) {
-            return new AnalysisResult("Тип сна", "Сова");
-        } else if (javor > sov && javor > golub) {
-            return new AnalysisResult("Тип сна", "Жаворонок");
+    // Вспомогательный метод: определяет тип для одного сеанса
+    private String classifySession(SleepSession session) {
+        LocalTime start = session.start().toLocalTime();
+        LocalTime end = session.end().toLocalTime();
+
+        if (start.isAfter(LocalTime.of(23, 0)) && end.isAfter(LocalTime.of(9, 0))) {
+            return "Сова";
+        } else if (start.isBefore(LocalTime.of(22, 0)) && end.isBefore(LocalTime.of(7, 0))) {
+            return "Жаворонок";
         } else {
-            return new AnalysisResult("Тип сна", "Голубь");
+            return "Голубь";
         }
     }
 }
