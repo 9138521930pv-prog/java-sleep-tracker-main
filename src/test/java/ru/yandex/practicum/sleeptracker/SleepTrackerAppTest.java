6e6f5bfeac429ffd605daae9b1d1a738e1a462bd
9138521930pv-prog <9138521930.pv@gmail.com>
2025-12-29 23:10:13 +0700

    Форматирование кода

--- src/test/java/ru/yandex/practicum/sleeptracker/SleepTrackerAppTest.java
+++ src/test/java/ru/yandex/practicum/sleeptracker/SleepTrackerAppTest.java
@@ -80,9 +80,9 @@ public class SleepTrackerAppTest {
     void testSleepTypeDetectsLark() {
         assertEquals("Жаворонок", testSleepType.apply(SleepTrackerAppTest.LARK_DOMINANT).value());
     }
-    
+
     @Test
-    void tieGoesToPigeon() {
+    void testSieGoesToPigeon() {
         assertEquals("Голубь", testSleepType.apply(SleepTrackerAppTest.MIXED_TYPES).value());
     }
 
